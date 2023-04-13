28ac7556ce50529404889efb79131fb8917d02b0
Yashas M V <yashasmv50@gmail.com>
2023-04-13 13:12:02 +0530

    updated init.py

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,7 @@ import psycopg2
 app = Flask(__name__)
 postgres = psycopg2.connect(
         host="dpg-cgrq4qpmbg5e4khqmrk0-a.oregon-postgres.render.com",
-        database="workshop_fh5q_user",
+        database="workshop_fh5q",
         user="workshop_fh5q_user",
         password="Xv33JEpyObqdbxQ5eYPiYbDu0uzJr5hS")
 # To prevent from using a blueprint, we use a cyclic import
